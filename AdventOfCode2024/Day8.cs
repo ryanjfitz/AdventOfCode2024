bdc088e08f6e4013b36235f104a98d87e499f5ec
Ryan Fitzgerald <ryanjfitz@gmail.com>
2024-12-18 16:59:37 -0600

    Day 8

---
+++ AdventOfCode2024/Day8.cs
@@ -0,0 +1,97 @@
+using System.Collections.Concurrent;
+
+namespace AdventOfCode2024;
+
+public static class Day8
+{
+    public static int GetAntinodeCount(string input)
+    {
+        var grid = input.ToTwoDimensionalCharArray();
+
+        var antennas = GetAntennas(grid);
+
+        var antinodes = GetAntinodes(grid, antennas);
+
+        return antinodes.Count();
+    }
+
+    private static IEnumerable<Antenna> GetAntennas(char[,] grid)
+    {
+        ConcurrentDictionary<char, Antenna> antennas = [];
+
+        for (int i = 0; i <= grid.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= grid.GetUpperBound(1); j++)
+            {
+                if (char.IsLetter(grid[i, j]) || char.IsDigit(grid[i, j]))
+                {
+                    char frequency = grid[i, j];
+
+                    antennas.GetOrAdd(frequency, _ => new Antenna(frequency, [])).Positions.Add(new Position(i, j));
+                }
+            }
+        }
+
+        return antennas.Values;
+    }
+
+    private static IEnumerable<Position> GetAntinodes(char[,] grid, IEnumerable<Antenna> antennas)
+    {
+        HashSet<Position> antinodes = [];
+
+        foreach (var antenna in antennas)
+        {
+            foreach (var antennaPositions in antenna.Positions.GetPermutations().Select(p => p.ToArray()))
+            {
+                for (int i = 0; i < antennaPositions.Length - 1; i++)
+                {
+                    Position antennaPositionOne = antennaPositions[i];
+                    Position antennaPositionTwo = antennaPositions[i + 1];
+
+                    Position diff = antennaPositionTwo - antennaPositionOne;
+
+                    var antinodeCandidateOne = antennaPositionOne - diff;
+                    if (antinodeCandidateOne != antennaPositionOne &&
+                        antinodeCandidateOne != antennaPositionTwo &&
+                        antinodeCandidateOne.IsWithinBounds(grid))
+                    {
+                        antinodes.Add(antinodeCandidateOne);
+                    }
+
+                    var antinodeCandidateTwo = antennaPositionTwo + diff;
+                    if (antinodeCandidateTwo != antennaPositionOne &&
+                        antinodeCandidateTwo != antennaPositionTwo &&
+                        antinodeCandidateTwo.IsWithinBounds(grid))
+                    {
+                        antinodes.Add(antinodeCandidateTwo);
+                    }
+                }
+            }
+        }
+
+        return antinodes;
+    }
+
+    private record Antenna(char Frequency, IList<Position> Positions);
+
+    private record Position(int I, int J)
+    {
+        public static Position operator +(Position a, Position b)
+        {
+            return new Position(a.I + b.I, a.J + b.J);
+        }
+
+        public static Position operator -(Position a, Position b)
+        {
+            return new Position(a.I - b.I, a.J - b.J);
+        }
+
+        public bool IsWithinBounds(char[,] grid)
+        {
+            return I >= grid.GetLowerBound(0) &&
+                   J >= grid.GetLowerBound(1) &&
+                   I <= grid.GetUpperBound(0) &&
+                   J <= grid.GetUpperBound(1);
+        }
+    }
+}
--- AdventOfCode2024/Extensions.cs
+++ AdventOfCode2024/Extensions.cs
@@ -28,4 +28,17 @@ public static class Extensions
 
         return result;
     }
+
+    public static IEnumerable<IEnumerable<T>> GetPermutations<T>(this ICollection<T> list, int? length = null)
+    {
+        length ??= list.Count;
+
+        if (length == 1)
+        {
+            return list.Select(t => new[] { t });
+        }
+
+        return GetPermutations(list, length - 1)
+            .SelectMany(t => list.Where(e => !t.Contains(e)), (t1, t2) => t1.Concat([t2]));
+    }
 }
