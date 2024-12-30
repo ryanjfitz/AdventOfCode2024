b3c68bfefe2ef62e04a703eea3908f08dfc210a8
Ryan Fitzgerald <ryanjfitz@gmail.com>
2024-12-30 10:48:23 -0600

    Day 8 Part 2

--- AdventOfCode2024/Day8.cs
+++ AdventOfCode2024/Day8.cs
@@ -4,13 +4,13 @@ namespace AdventOfCode2024;
 
 public static class Day8
 {
-    public static int GetAntinodeCount(string input)
+    public static int GetAntinodeCount(string input, bool part2 = false)
     {
         var grid = input.ToTwoDimensionalCharArray();
 
         var antennas = GetAntennas(grid);
 
-        var antinodes = GetAntinodes(grid, antennas);
+        var antinodes = part2 ? GetAntinodesPart2(grid, antennas) : GetAntinodes(grid, antennas);
 
         return antinodes.Count();
     }
@@ -93,5 +93,48 @@ public static class Day8
                    I <= grid.GetUpperBound(0) &&
                    J <= grid.GetUpperBound(1);
         }
+    }
+
+    private static IEnumerable<Position> GetAntinodesPart2(char[,] grid, IEnumerable<Antenna> antennas)
+    {
+        HashSet<Position> antinodes = [];
+
+        foreach (var antenna in antennas)
+        {
+            foreach (var antennaPositions in antenna.Positions.GetPermutations().Select(p => p.ToArray()))
+            {
+                for (int i = 0; i < antennaPositions.Length - 1; i++)
+                {
+                    Position antennaPositionOne = antennaPositions[i];
+                    Position antennaPositionTwo = antennaPositions[i + 1];
+
+                    Position diff = antennaPositionTwo - antennaPositionOne;
+
+                    var antinodeCandidateOne = antennaPositionOne;
+                    do
+                    {
+                        if (antinodeCandidateOne.IsWithinBounds(grid))
+                        {
+                            antinodes.Add(antinodeCandidateOne);
+                        }
+
+                        antinodeCandidateOne -= diff;
+                    } while (antinodeCandidateOne.IsWithinBounds(grid));
+
+                    var antinodeCandidateTwo = antennaPositionTwo;
+                    do
+                    {
+                        if (antinodeCandidateTwo.IsWithinBounds(grid))
+                        {
+                            antinodes.Add(antinodeCandidateTwo);
+                        }
+
+                        antinodeCandidateTwo += diff;
+                    } while (antinodeCandidateTwo.IsWithinBounds(grid));
+                }
+            }
+        }
+
+        return antinodes;
     }
 }
