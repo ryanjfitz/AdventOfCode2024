6a5d18dddead0587e9e0d5973f8629d4ccb0cecb
Ryan Fitzgerald <ryanjfitz@gmail.com>
2026-02-24 11:38:56 -0600

    WIP

--- AdventOfCode2024.Tests/Day16Tests.cs
+++ AdventOfCode2024.Tests/Day16Tests.cs
@@ -172,6 +172,18 @@ public class Day16Tests
             [new Point(2, 1), new Point(1, 1), new Point(1, 2), new Point(1, 3), new Point(2, 3)],
             [new Point(2, 1), new Point(3, 1), new Point(3, 2), new Point(3, 3), new Point(2, 3)]
         ]);
+        yield return () => ("""
+                            #######
+                            #..E..#
+                            #.#.#.#
+                            #..S..#
+                            #######
+                            """,
+        [
+            [new Point(3, 3), new Point(2, 3), new Point(1, 3)],
+            [new Point(3, 3), new Point(3, 2), new Point(3, 1), new Point(2, 1), new Point(1, 1), new Point(1, 2), new Point(1, 3)],
+            [new Point(3, 3), new Point(3, 4), new Point(3, 5), new Point(2, 5), new Point(1, 5), new Point(1, 4), new Point(1, 3)]
+        ]);
     }
 
     [Test]
